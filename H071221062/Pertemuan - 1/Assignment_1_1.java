d5b02afdbe4ca2ad8f26b31dcb0b29e2fc32529a
ilyfauzan <ahmdfauzanrr@gmail.com>
2023-03-12 11:37:13 +0800

    Perbaikan tugas praktikum pertemuan - 1

--- H071221062/Pertemuan - 1/Assignment_1_1.java
+++ H071221062/Pertemuan - 1/Assignment_1_1.java
@@ -1,20 +1,19 @@
+import java.util.Scanner;
+
 public class Assignment_1_1 {
 
     public static void main(String[] args) {
 
         Scanner sc = null;
         try {
-            sc = new Scanner(System.in); // unutk membaca input dari user
-            System.out.print("NIM : "); // inputan user yang memiliki variabel NIM
-            String NIM = sc.nextLine(); // membaca input dari user
-            System.out.print("SOAL : No."); // inputan user yang memiliki variabel tersebut
+            sc = new Scanner(System.in);
+            System.out.print("NIM : ");
+            String NIM = sc.nextLine();
+            System.out.print("SOAL : No.");
             int angka = Integer.parseInt(NIM.substring(NIM.length() - 3));
-            // fungsi Integer.ParseInt untuk mengubah tipedata string menjadi integer
-            // untuk mengambil 3 nilai terakhir terhadap sebuah string
-
             System.out.println(angka % 7 == 0 ? 7 : angka % 7);
-            // jika 3 nilai terakhir inputan modulu 7 == 0 akan mengembalikan nilai 7
-            // jika tidak maka mengembalikan nilai modulu tersebut
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             if (sc != null) {
                 sc.close();
--- H071221062/Pertemuan - 1/Assignment_1_3.java
+++ H071221062/Pertemuan - 1/Assignment_1_3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task3 {
+public class Assignment_1_3 {
     public static void main(String[] args) {
        
         Scanner sc = new Scanner(System.in);
